235ae72f3973c97a0af7578e9bd2984867693510
Jirawat Boonkumnerd <jo06942@gmail.com>
2023-10-26 14:16:23 +0700

    feat: add cdragon data downloader

---
+++ download_assets.py
@@ -0,0 +1,45 @@
+import requests
+import json 
+import os
+
+def cdragonPath(path: str) :
+    return path.lower().replace("/lol-game-data/assets/", "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/")
+
+def downlaodIntoPath(filePath: str, downloadPath: str):
+
+    os.makedirs(os.path.dirname(filePath), exist_ok=True)
+
+    print(f"downloading {downloadPath}")
+
+    r = requests.get(downloadPath, allow_redirects=True)
+    open(filePath, 'wb').write(r.content)
+
+
+print("loading communitydragon data")
+
+setMutator = "TFTSet9_Stage2"
+jsonDir = "./src/data/"
+staticDir = "./static/"
+
+res = requests.get('https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-summary.json')
+champions = json.loads(res.text)
+for c in champions:
+    if c["id"] > 0 and c["squarePortraitPath"]:
+        # download tile image
+        path = c["squarePortraitPath"].lower()
+        downlaodIntoPath(staticDir + path, cdragonPath(path))
+
+res = requests.get('https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/summoner-spells.json')
+spells = json.loads(res.text)
+for s in spells:
+    if c["id"] > 0 and c["iconPath"]:
+        # download tile image
+        path = c["iconPath"].lower()
+        downlaodIntoPath(staticDir + path, cdragonPath(path))
+
+with open(jsonDir + "champions.json", "w") as outfile:
+    outfile.write(json.dumps(champions, indent=4))
+
+with open(jsonDir + "spells.json", "w") as outfile:
+    outfile.write(json.dumps(spells, indent=4))
+
